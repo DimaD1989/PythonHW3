3cd71a3d4eb0172d9bee7696aed8c6534e50354b
DimaD1989 <d_d1989sv@mail.ru>
2022-10-06 23:22:45 +0300

    end

--- 1.py
+++ 1.py
@@ -28,58 +28,23 @@
 # которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
-list_num =[3.3, 5.1, 1.01]
-ost_list = []
-for i in range(len(list_num)):
-    a = int(list_num[i])
-    b= round(list_num[i] - int(list_num[i]), 2)
-    ost_list.append(b)
-    c_min = ost_list[0]
-    c_max = ost_list[0]
-    for j in ost_list:
-        if j < c_min:
-            c_min=j
-        else:
-            if j>c_max:
-                c_max=j
-d=c_max-c_min
+# list_num =[1.1, 1.2, 3.1, 5, 10.01]
+# ost_list = []
+# for i in range(len(list_num)):
+#     a = int(list_num[i])
+#     b= round(list_num[i] - int(list_num[i]), 2)
+#     ost_list.append(b)
+#     c_min = ost_list[0]
+#     c_max = ost_list[0]
+#     for j in ost_list:
+#         if j < c_min:
+#             c_min=j
+#         else:
+#             if j>c_max:
+#                 c_max=j
+# d=c_max-c_min
 
-print(d)
-
-
-# a=list_mult = [float('0' + str(list)[str(list).index('.'):])]
-# for i in range(len(list)):
-#     math.modf(len(list))
-# list_mult.append(x)
-# print(list_mult)
-# x = float(input())
-# y = x-math.floor(x)
-# print(y)
-# from math import *
-
-# x = int(float(input('Введите ряд вещественных чисел через пробел: '))).split()
-# if x > 1:
-#     a = floor(x)
-#     b = x - a
-#     print(round(b, 2))
-# else:
-#     print(x)
-# a = 1.45678
-# b = float('0.%s' % str(a).split('.')[1])
-# print(b)
-
-# def sum_number(n: float) -> int:
-#     num=int(str(n).replace('.', ''))
-#     sum = 0
-#     while num != 0:
-#         sum = sum + num%10
-#         # num //= 10
-#     return sum
-
-# n = float(input('Введите вещественное число: '))
-# sum = sum_number(n)
-# print(sum)
-
+# print(d)
 
 # Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 # Пример:
